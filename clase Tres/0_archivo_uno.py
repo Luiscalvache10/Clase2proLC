679dab74555d15f309e717d41c4554dd31f076de
Luiscalvache10 <167647548+Luiscalvache10@users.noreply.github.com>
2024-04-27 19:08:45 -0500

    clase 3
    hooladsfdfdfdfdfdfdf

---
+++ clase Tres/0_archivo_uno.py
@@ -0,0 +1,16 @@
+#esta es una variable 
+variable = 8 
+variable_dos = "texto dos"
+my_string = "thisStringAwesome"
+
+print(my_string.upper())
+print(my_string)
+
+print(variable_dos)
+#print() permite mostrar en la terminal la información
+"""
+#print("hola")
+#print(type(8))
+#print(type(1.8))
+#print(type("texto"))
+"""
